be5f366f8eb9346cca95a10ea91463b361a9034c
Plandel <kaioplandel@gmail.com>
2024-10-24 13:06:08 -0300

    feat: add colors

---
+++ lib/utils/AppColors.dart
@@ -0,0 +1,9 @@
+import 'dart:ui';
+
+class AppColors {
+  static const Color darkNavyBlue = Color(0xFF020206);
+  static const Color lightPurple = Color(0xFFBA83DE);
+  static const Color pinkishPurple = Color(0xFFDE83B0);
+  static const Color darkGray = Color(0xFF1F1F1F);
+  static const Color mediumGray = Color(0xFF4F4F4F);
+}
